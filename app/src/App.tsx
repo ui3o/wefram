f280abffa08a244bbe6e19c7952a8fbd5d9a27dc
ui3o.com <ui3o.com@gmail.com>
2024-07-16 13:13:09 +0200

    change to chakra-ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,24 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-import OutlinedCard from './Card';
+import { Box, Spinner, useToast } from "@chakra-ui/react";
+import * as React from "react";
+import * as Bowser from "bowser";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+const result = Bowser.getParser(window.navigator.userAgent);
 
 export default function App() {
+  const toast = useToast();
+  toast({
+    title: `Your operation system is ${result.getOSName()}`,
+    description: `You are using ${result.getBrowserName()} with version ${result.getBrowserVersion()}`,
+    status: "error",
+    duration: 9000,
+    isClosable: false,
+  });
+
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Create React App example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-        <OutlinedCard />
-      </Box>
-    </Container>
+    <Box sx={{ my: 4 }}>
+      <img src={`${process.env.PUBLIC_URL}/stage1.png`}></img>
+      We Farm app
+      <Spinner />
+    </Box>
   );
 }
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,17 +1,14 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import App from './App';
-import theme from './theme';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
+import { ChakraProvider } from "@chakra-ui/react";
+
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement!);
 
 root.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
+  <ChakraProvider>
     <App />
-  </ThemeProvider>,
+  </ChakraProvider>
 );
